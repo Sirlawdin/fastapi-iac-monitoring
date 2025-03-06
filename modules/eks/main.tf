2bb575b4acf44a69a665d8daa187870087e7a277
Salaudeen O. Abdulrasaq <82021949+Sirlawdin@users.noreply.github.com>
2025-03-06 09:07:33 +0100

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -60,3 +60,8 @@ resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
   role       = aws_iam_role.eks_worker_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
